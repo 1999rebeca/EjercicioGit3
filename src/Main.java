8ecf26a484445cffd01fb0e343f94254bb4a980e
rebeca <rebecabargiela@gmail.com>
2022-11-23 17:27:31 +0100

    añadiendo código para probar las propiedades de java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+        public static void main(String[] args) {
+
+            int numero1 = 0;
+            int numero2 = 0;
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Escribe el primer número :");
+            numero1 = sc.nextInt();
+
+            System.out.println("Escribe el segundo número :");
+            numero2 = sc.nextInt();
+
+            int resultado = numero2 + numero1;
+
+            System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado); }
 }
