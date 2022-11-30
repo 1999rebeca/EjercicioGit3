8243402655c1559a51fa5ebdf8c3c9bd88f18905
rebeca <rebecabargiela@gmail.com>
2022-11-30 16:36:05 +0100

    puse el codigo de ejemplo en el proyecto porq antes habia pueso una cosa random

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,13 @@ public class Main {
 
         public static void main(String[] args) {
 
-            int numero1 = 0;
-            int numero2 = 0;
+            Empleado p = new Empleado("Pepe Pérez", 30);
+            System.out.println(p);
 
-            Scanner sc = new Scanner(System.in);
+            p.nombre = "Otro nombre";
+/*
+        p.nombre = "Otro nombre";  // Si intentamos cambiar el campo nombre, nos da un error de compilación
+        System.out.println(p);
+*/
 
-            System.out.println("Escribe el primer número :");
-            numero1 = sc.nextInt();
-
-            System.out.println("Escribe el segundo número :");
-            numero2 = sc.nextInt();
-
-            int resultado = numero2 + numero1;
-
-            System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado); }
-}
+        }
